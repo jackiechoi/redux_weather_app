d6ccb0b917a9705580a3014333170c5630f38270
Jackie <jackiechoi66@gmail.com>
2017-05-12 16:59:02 +0200

    actioncreator implemented. Added Axios for httprequest

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
+import axios from 'axios';
+//axios library that acts like ajax and returns promise
+
 const API_KEY = '588c140c1c254b0e21bf776108fb88a5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+
+	return {
+		type: FETCH_WEATHER;
+		payload: request
+	};
+}
