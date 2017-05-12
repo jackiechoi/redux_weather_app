316e133072c531af7863606c7434201265a50931
Jackie <jackiechoi66@gmail.com>
2017-05-12 20:08:57 +0200

    search_bar completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
-//axios library that acts like ajax and returns promise
+//Axios is a js library that acts like jQuery, makes ajax requests and returns promise
 
 const API_KEY = '588c140c1c254b0e21bf776108fb88a5';
+//url for ajax get request
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
+//actionCreator called fetchWeather returns an object/Action, with a type.
+//it takes city, a string, and uses it as part of the search Query (as shown in const url)
 export function fetchWeather(city) {
 	const url = `${ROOT_URL}&q=${city},us`;
+	//axios makes the ajax request in the form of get, with the url supplied, and returns a promise. Then the promise is passed in payload property of the action.
 	const request = axios.get(url);
 
 	return {
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,31 +3,32 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
 
+
+//the goal here is to call the actionCreator whenever the user submits the form.
 class SearchBar extends Component {
-    //set state up and initialize it
-    constructor(props){
-        super(props);
+  //set state up and initialize it
+  constructor(props){
+    super(props);
 
-        this.state = { term: '' };
+    this.state = { term: '' };
 
-        //SearchBar(this) has functions, onInputChange, onFormSubmit. Bind those functions with this which is SearchBar and then replace the functions with the newly bound instances of SearchBar
-        this.onInputChange = this.onInputChange.bind(this);
-        this.onFormSubmit = this.onFormSubmit.bind(this);
-    }
+    //SearchBar(this) has functions, onInputChange, onFormSubmit. Bind those functions with "this" which is SearchBar and then replace the functions with the newly bound instances of SearchBar
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
 
     onInputChange(event){
-        console.log(event.target.value);
         this.setState({ term: event.target.value });
     }
 
+    //wire up the onFormSubmit function (callback function) so that whenever the user submits, we fire the actionCreator, fetchWeather in actions/index.js and make the API request with Axios
     onFormSubmit(event){
         event.preventDefault(); //don't submit the form. Just go and fetch the weather data!
 
-
         //2. Calls actionCreator(fetchWeather) with the search term
         this.props.fetchWeather(this.state.term);
-        //3. Then set the term to empty string again which makes the component re-render
-        this.setState({ term: '' }); //clear the search input
+        //3. Then set the term to empty string again which makes the component re-render and clears the search input
+        this.setState({ term: '' });
     }
 
     render (){
@@ -49,8 +50,13 @@ class SearchBar extends Component {
     }
 }
 
+//bind the actionCreator, fetWeather, as a property to the search_bar container
 function mapDispatchToProps(dispatch) {
+    //this makes sure that whenever the actionCreator, fetchWeather, is called and return an action, is bound with dispatch. Resulting in the action to flow down into the middleware and the reducer
     return bindActionCreators({ fetchWeather }, dispatch);
 }
 
+//Passing null as the first arg, because state is not needed here.
 export default connect(null, mapDispatchToProps)(SearchBar);
+
+//by binding the actionCreator to dispatch and mapping it to props, this gives us access to function this.props.fetchWeather inside the component here (#2)
